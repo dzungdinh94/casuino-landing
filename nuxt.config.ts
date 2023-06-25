c69e32e567ca47c077dab39515fd9340e730463e
Jame Dzung <dung.nguyen@sleek.com>
2023-06-25 20:30:25 +0700

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,28 @@ export default defineNuxtConfig({
   css: [`~/style.css`],
   devtools: { enabled: true },
   modules: ['@nuxtjs/google-fonts',],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Jura:wght@300;400;500;600;700&display=swap"
+        }
+      ]
+    }
+  },
   googleFonts: {
     display: 'swap',
     inject: true,
     download: true,
     families: {
-      Inter: [100,200,300,400,500,600,700,800,900],
+      Inter: [100, 200, 300, 400, 500, 600, 700, 800, 900],
       Jura: {
-        wght: [300,400,500,600,700],
+        wght: [300, 400, 500, 600, 700],
       },
     }
   },
